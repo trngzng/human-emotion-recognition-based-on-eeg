62679f27f6aeeced6594df5c1f436dd125e3892e
Giang Phan Truong <phangiang182@gmail.com>
2025-04-19 18:51:03 +0700

    update: Add the register address for the channels mapping

--- src/firmware/stm32/ver-1/DeviceDrivers/AD7768/drv_ad7768_reg.h
+++ src/firmware/stm32/ver-1/DeviceDrivers/AD7768/drv_ad7768_reg.h
@@ -20,7 +20,9 @@
 /* Public defines ----------------------------------------------------- */
 
 // Register addresses
-#define AD7768_CH_MODE               0x01
+#define AD7768_CH_STANDBY            0x00
+#define AD7768_CH_MODE_A             0x01
+#define AD7768_CH_MODE_B             0x02
 #define AD7768_POWER_MODE            0x04
 #define AD7768_DATA_CONTROL          0x06
 #define AD7768_INTERFACE_CFG         0x07
