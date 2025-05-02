ebd1a9f95f814421967b492ca58df192c9d1ac5a
Giang Phan Truong <phangiang182@gmail.com>
2025-05-02 16:27:38 +0700

    feat: Implement the function to create the Topic in the PubSub pattern

---
+++ src/firmware/stm32/ver-1/SystemServices/DataManager/sys_data_mng.c
@@ -0,0 +1,123 @@
+/**
+ * @file       sys_data_mng.c
+ * @copyright  Copyright (C) 2024 GNG. All rights reserved.
+ * @license    None.
+ * @version    1.0.0
+ * @date       2025-05-02
+ * @author     Giang Phan
+ *             
+ * @brief      Handle the system data manager based on the PubSub pattern.
+ *             
+ * @note          
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "sys_data_mng.h"
+
+/* Private defines ---------------------------------------------------- */
+#define SYS_DATA_MNG_BUFF_SIZE  (255) /*!< Description of PRIVATE_DEFINE_1 */
+
+/* Private enumerate/structure ---------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+typedef enum 
+{
+  PRIVATE_ENUM_1, /**< Description of PRIVATE_ENUM_1 */
+  PRIVATE_ENUM_2, /**< Description of PRIVATE_ENUM_2 */
+  PRIVATE_ENUM_3  /**< Description of PRIVATE_ENUM_3 */
+} private_enum_type_t;
+
+/**
+ * @brief <structure descriptiton>
+ */
+typedef struct 
+{
+  SYS_DATA_MNG_IDTopicTypeDef id;     /**< Description of member_1 */
+  CB_HandleTypeDef cbuf;
+  uint32_t num_of_subscribers;        /**< Description of member_2 */
+  uint32_t message_size;
+} SYS_DATA_MNG_TopicTypeDef;
+
+/* Private macros ----------------------------------------------------- */
+/**
+ * @brief  <macro description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+#define PRIVATE_MACRO(a)  do_something_with(a)
+
+/* Public variables --------------------------------------------------- */
+int g_var_1;
+int g_var_2;
+
+/* Private variables -------------------------------------------------- */
+static SYS_DATA_MNG_TopicTypeDef topics[SYS_DATA_MNG_MAX_TOPIC];    /**< The system data manager topics */
+
+static uint8_t sMsgBroker[SYS_DATA_MNG_BUFF_SIZE];                  /**< The message buffer for the topics */
+static uint32_t sMsgBrokerTopPtr = 0;                               /**< The pointer to the top of the message buffer */
+static uint8_t sCbufReadBuffer[SYS_DATA_MNG_BUFF_SIZE];             /**< Buffer to store data from circular buffer */
+static SYS_DATA_MNG_IDTopicTypeDef sNumOfTopics = 0;                /**< Current number of topics in the system data */
+
+/* Private function prototypes ---------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+static void private_function(void);
+/* Function definitions ----------------------------------------------- */
+BaseStatusTypeDef SYS_DATA_MNG_CreateTopic(SYS_DATA_MNG_IDTopicTypeDef id, uint32_t msg_size, uint32_t num_of_msg)
+{
+  BaseStatusTypeDef ret = BS_ERROR;
+  uint32_t i = 0;
+
+  __ASSERT(id < SYS_DATA_MNG_MAX_TOPIC, BS_ERROR);
+  __ASSERT(sNumOfTopics < SYS_DATA_MNG_MAX_TOPIC, BS_ERROR);
+
+  for (uint_fast8_t i = 0; i < SYS_DATA_MNG_MAX_TOPIC; i++)
+    __ASSERT(topics[i].id != id, BS_ERROR);
+
+  __ASSERT(topics[id].cbuf.active == BS_FALSE, BS_ERROR);
+  __ASSERT(msg_size > 0, BS_ERROR);
+  __ASSERT(num_of_msg > 0, BS_ERROR);
+  __ASSERT((msg_size * num_of_msg) <= (SYS_DATA_MNG_BUFF_SIZE - sMsgBrokerTopPtr), BS_ERROR);
+
+  ret = CB_Init(&topics[id].cbuf, sMsgBroker, msg_size * num_of_msg);
+  __ASSERT(ret == CB_SUCCESS, BS_ERROR);
+
+  topics[id].num_of_subscribers = 0;
+  topics[id].message_size = msg_size;
+  topics[id].id = id;
+
+  sMsgBrokerTopPtr += (msg_size * num_of_msg);
+  sNumOfTopics++;
+
+  return BS_OK;
+}
+
+/* Private definitions ------------------------------------------------ */
+static void private_function(void)
+{
+}
+/* End of file -------------------------------------------------------- */
---
+++ src/firmware/stm32/ver-1/SystemServices/DataManager/sys_data_mng.h
@@ -0,0 +1,61 @@
+/**
+ * @file       sys_data_mng.h
+ * @copyright  Copyright (C) 2024 GNG. All rights reserved.
+ * @license    None.
+ * @version    1.0.0
+ * @date       2025-05-02
+ * @author     Giang Phan
+ *             
+ * @brief      Handle the system data manager based on the PubSub pattern.
+ *             
+ * @note       None. 
+ * @example    None.
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __SYS_DATA_MNG_H
+#define __SYS_DATA_MNG_H
+
+/* Includes ----------------------------------------------------------- */
+#include "main.h"
+#include "cbuffer.h"
+#include "common.h"
+/* Public defines ----------------------------------------------------- */
+#define SYS_DATA_MNG_MAX_SUBSCRIBERS  (10)  /**< The maximum number of subscribers for a topic */
+
+typedef void (*SYS_DATA_MNG_CbFunc)(uint8_t *, uint32_t); /**< The callback function type for the subscribers */
+/* Public enumerate/structure ----------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+typedef enum 
+{
+  SYS_DATA_MNG_TOPIC_ADC_DSP = 0x00,  /**< Description of PUBLIC_ENUM_1 */
+  SYS_DATA_MNG_TOPIC_ADC_USB,         /**< Description of PUBLIC_ENUM_1 */
+  SYS_DATA_MNG_TOPIC_DSP_USB,         /**< Description of PUBLIC_ENUM_1 */
+  SYS_DATA_MNG_MAX_TOPIC
+} SYS_DATA_MNG_IDTopicTypeDef;
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  Initialize the system data manager
+ *
+ * @param[in]     id          The ID of the topic to be created
+ * @param[in]     msg_size    The size of the message in bytes
+ * @param[in]     num_of_msg  The number of messages of this topic
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - BS_OK: Success
+ *  - BS_ERROR: Error
+ */
+BaseStatusTypeDef SYS_DATA_MNG_CreateTopic(SYS_DATA_MNG_IDTopicTypeDef id, uint32_t msg_size, uint32_t num_of_msg);
+
+#endif // __SYS_DATA_MNG_H
+
+/* End of file -------------------------------------------------------- */
