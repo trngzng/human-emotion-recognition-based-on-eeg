1a6dc95f793caa851b5b858c26b170f45b8b5791
Giang Phan Truong <phangiang182@gmail.com>
2025-06-08 00:20:45 +0700

    update: Apply digitla filter to the signal

--- src/firmware/stm32/ver-1/SystemServices/AcquisitionManager/sys_acquisition_mng.c
+++ src/firmware/stm32/ver-1/SystemServices/AcquisitionManager/sys_acquisition_mng.c
@@ -18,6 +18,7 @@
 #include "sys_serial.h"
 #include "bsp_dwt.h"
 #include <string.h>
+#include "filter.h"
 /* Private defines ---------------------------------------------------- */
 
 /* Private enumerate/structure ---------------------------------------- */
@@ -27,8 +28,65 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-static int32_t rawSamples[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL][SYS_ACQ_MNG_MAX_EEG_SAMPLES]; /**< Raw samples from ADC */
-static float filteredSamples[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL][SYS_ACQ_MNG_MAX_EEG_SAMPLES]; /**< Samples after the filter processing */
+static float rawSamples[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL]; /**< Raw samples from ADC */
+static float filteredSamples[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL]; /**< Samples after the filter processing */
+
+const float firCoeff100HzSampling1000[51] = {6.23707493203100e-19,
+                                             0.000651936536801578,
+                                             0.00124319314520673,
+                                             0.00154534133131494,
+                                             0.00121969027941796,
+                                             -1.30863330037917e-18,
+                                             -0.00201548144449766,
+                                             -0.00415052127829799,
+                                             -0.00522690670351652,
+                                             -0.00402425886959341,
+                                             3.10179234333809e-18,
+                                             0.00606470479754808,
+                                             0.0119023545680876,
+                                             0.0143515447823021,
+                                             0.0106508925575157,
+                                             -5.31825881490377e-18,
+                                             -0.0152975048205391,
+                                             -0.0297614666016229,
+                                             -0.0360342519662000,
+                                             -0.0272952563088151,
+                                             7.11141785786268e-18,
+                                             0.0441130473576423,
+                                             0.0976505458279451,
+                                             0.149177852216505,
+                                             0.186419208415108,
+                                             0.200000000000000,
+                                             0.186419208415108,
+                                             0.149177852216505,
+                                             0.0976505458279451,
+                                             0.0441130473576423,
+                                             7.11141785786268e-18,
+                                             -0.0272952563088151,
+                                             -0.0360342519662000,
+                                             -0.0297614666016229,
+                                             -0.0152975048205391,
+                                             -5.31825881490377e-18,
+                                             0.0106508925575157,
+                                             0.0143515447823021,
+                                             0.0119023545680876,
+                                             0.00606470479754808,
+                                             3.10179234333809e-18,
+                                             -0.00402425886959341,
+                                             -0.00522690670351652,
+                                             -0.00415052127829800,
+                                             -0.00201548144449766,
+                                             -1.30863330037917e-18,
+                                             0.00121969027941796,
+                                             0.00154534133131494,
+                                             0.00124319314520673,
+                                             0.000651936536801578,
+                                             6.23707493203100e-19};
+
+static FilterHandleTypeDef notchFilter50HzChannel[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL];
+static FilterHandleTypeDef lowPassFilter100HzChannel[SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL];
+
+
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
@@ -39,36 +97,31 @@ BaseStatusTypeDef SYS_ACQ_MNG_Init(void)
   ret = BSP_EXT_ADC_Init();
   __ASSERT(ret == BS_OK, BS_ERROR); // Check if the ADC initialization was successful
 
+  for (uint_fast8_t i = 0; i < SYS_ACQ_MNG_NUM_OF_EEG_CHANNEL; i++)
+  {
+    FILTER_Init(&notchFilter50HzChannel[i], NOTCH_FILTER, 2, 1000, 50, NULL, NULL);
+    FILTER_Init(&lowPassFilter100HzChannel[i], LOW_PASS_FILTER, 50, 1000, 100, NULL, firCoeff100HzSampling1000);
+  }
+
   return BS_OK;   
 }
 
 void SYS_ACQ_MNG_ProcessData(uint8_t *data, uint32_t size)
 {
-//  static uint8_t sampleCount = 0;
+  float tmp = 0;
 
 //  UNUSED(size);
 
-//  for (uint_fast8_t ch = 0; ch < 2; ch++)  // 2 channel
-//  {
-//    uint8_t *p = &data[ch * 4];
-//    uint32_t raw24Bit = (p[3] << 24) | (p[2] << 16) | (p[1] << 8) | p[0];
-//
-//    // Sign extend 24-bit to 32-bit signed
-//    if ((raw24Bit & 0x800000) == 0x800000)
-//      raw24Bit |= 0xFF000000;
-//
-//    rawSamples[ch][sampleCount] = (int32_t)raw24Bit;
-//
-////    rawSamples[ch][sampleCount] = raw24Bit;
-//
-//  }
-//
-//  if (++sampleCount == SYS_ACQ_MNG_MAX_EEG_SAMPLES)
-//  {
-//    sampleCount = 0;
-    SYS_DATA_MNG_PublishMsg(SYS_DATA_MNG_TOPIC_ACQ_SYS_TO_USB, data, size);
-//    SYS_DATA_MNG_PublishMsg(SYS_DATA_MNG_TOPIC_ACQ_SYS_TO_USB, (uint8_t *)filteredSamples, sizeof(filteredSamples));
-//  }
+   memcpy(rawSamples, data, sizeof(rawSamples));
+
+   for (uint_fast8_t i = 0; i < 2; i++)
+   {
+     tmp = FILTER_Process(&notchFilter50HzChannel[i], rawSamples[i]);
+     filteredSamples[i] = FILTER_Process(&lowPassFilter100HzChannel[i], tmp);
+   }
+
+  SYS_DATA_MNG_PublishMsg(SYS_DATA_MNG_TOPIC_ACQ_SYS_TO_USB, (uint8_t *)rawSamples, sizeof(rawSamples));
+//  SYS_DATA_MNG_PublishMsg(SYS_DATA_MNG_TOPIC_ACQ_SYS_TO_USB, (uint8_t *)filteredSamples, sizeof(filteredSamples));
 }
 /* Private definitions ------------------------------------------------ */
 
